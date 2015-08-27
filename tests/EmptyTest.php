678a43995434e07458b01dbda56ee82afbabe3a8
v.gryaznov <v.gryaznov@interkassa.com>
2015-08-27 17:47:51 +0300

    init empty tests

---
+++ tests/EmptyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Messenger;
+
+
+class EmptyTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testTrue()
+    {
+      
+        $this->assertTrue(!! true);
+    }
+
+}
